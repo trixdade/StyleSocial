2e7b1f86e3525a123d33b227b75b70eaef0a12bb
unknown <roma.bezaev@yandex.ru>
2023-04-24 16:14:35 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Clothes recommender sytem based on user social profile.',
+    author='Roman Bezaev',
+    license='MIT',
+)
